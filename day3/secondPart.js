832381a89b75c60551ffc7c93c22de9a3a96a33b
joaquin1990 <joaquingarese@gmail.com>
2022-12-19 15:37:53 -0300

    Second part

--- day3/secondPart.js
+++ day3/secondPart.js
@@ -1,111 +1,77 @@
-// A - X = Rock (1pts)
-// B - Y = Paper (2pts)
-// C - Z = Scissors (3pts)
-
-//  "X" means i need to loose the round
-//  "Y" means i need to round to end in draw
-//  "Z" means i needd to win the round
-
+// Put all the rusucks inside an array:
 const fs = require("fs");
 const { arrayBuffer } = require("stream/consumers");
 const data = fs.readFileSync("./file.txt", { encoding: "utf8", flag: "r" });
 const dataToArray = [...data];
-const cleanedArray = [];
+// console.log(dataToArray);
 
-for (let x = 0; x < dataToArray.length; x++) {
-  if (
-    dataToArray[x] === "A" ||
-    dataToArray[x] === "B" ||
-    dataToArray[x] === "C" ||
-    dataToArray[x] === "X" ||
-    dataToArray[x] === "Y" ||
-    dataToArray[x] === "Z"
-  ) {
-    cleanedArray.push(dataToArray[x]);
+// Function that separate the different rucksucks in an array:
+let rucksuck = [];
+let arrayOfRucksucks = [];
+let rucksuck2;
+function separateRucksucks() {
+  for (let x = 0; x < dataToArray.length; x++) {
+    if (dataToArray[x] != "\n") {
+      rucksuck.push(dataToArray[x]);
+      // rucksuck2 = rucksuck.toString();
+    }
+    if (dataToArray[x] === "\n" || x == dataToArray.length - 1) {
+      rucksuck2 = rucksuck.join("");
+      arrayOfRucksucks.push(rucksuck2);
+      rucksuck = [];
+    }
   }
+  return arrayOfRucksucks;
+}
+separateRucksucks();
+
+// Setting Lowercase letters values:
+function lowercaseValues(v) {
+  let findindex = alphabetLowercase.findIndex((x) => x === v);
+  let valueLowercase = findindex + 1;
+  return valueLowercase;
 }
 
-let sumatoria2 = 0;
-let contador = 0;
+// Setting Uppercase letters values:
+function uppercaseValues(V) {
+  let findindexUpper = alphabet.findIndex((x) => x === V);
+  let valueUppercase = findindexUpper + 1 + 26;
+  return valueUppercase;
+}
 
-for (let x = 0; x < 2501; x++) {
-  // "X" Need to loose:
-  if (cleanedArray[1] === "X") {
-    switch (cleanedArray[0]) {
-      case "A":
-        sumatoria2 += 0 + 3;
-        contador++;
-        cleanedArray.splice(0, 2);
-        continue;
-      case "B":
-        sumatoria2 += 0 + 1;
-        contador++;
-        cleanedArray.splice(0, 2);
-        break;
-      case "C":
-        sumatoria2 += 0 + 2;
-        contador++;
-        cleanedArray.splice(0, 2);
-        break;
-      default:
-        break;
-    }
-  }
+// Now we have to go through 3 arrays, and find the only letter that is repeated:
 
-  //  "Y" Need to draw:
-  if (cleanedArray[1] === "Y") {
-    switch (cleanedArray[0]) {
-      case "A":
-        sumatoria2 += 3 + 1;
-        contador++;
-        cleanedArray.splice(0, 2);
-        // continue;
-        break;
-      case "B":
-        sumatoria2 += 3 + 2;
-        contador++;
-        cleanedArray.splice(0, 2);
-        // continue;
-        break;
-      case "C":
-        sumatoria2 += 3 + 3;
-        contador++;
-        cleanedArray.splice(0, 2);
-        // continue;
-        break;
-      default:
-        break;
-    }
-  }
-
-  //   "Z" need to win
-  if (cleanedArray[1] === "Z") {
-    switch (cleanedArray[0]) {
-      case "A":
-        sumatoria2 += 6 + 2;
-        contador++;
-        cleanedArray.splice(0, 2);
-        // continue;
-        break;
-      case "B":
-        sumatoria2 += 6 + 3;
-        contador++;
-        cleanedArray.splice(0, 2);
-        // continue;
-        break;
-      case "C":
-        sumatoria2 += 6 + 1;
-        contador++;
-        cleanedArray.splice(0, 2);
-        // continue;
-        break;
-      default:
-        break;
+const arrayOfBadges = [];
+function findingbadge() {
+  let contador = 0;
+  for (let f = 0; f < 101; f++) {
+    for (let x = 0; x < arrayOfRucksucks[0].length; x++) {
+      if (contador >= 2) {
+        contador = 0;
+      }
+      for (let i = 0; i < arrayOfRucksucks[1].length; i++) {
+        if (contador >= 2) {
+          break;
+        }
+        if (arrayOfRucksucks[0][x] === arrayOfRucksucks[1][i]) {
+          contador++;
+          for (let m = 0; m < arrayOfRucksucks[2].length; m++) {
+            if (arrayOfRucksucks[1][i] === arrayOfRucksucks[2][m]) {
+              contador++;
+              if (contador >= 2) {
+                arrayOfBadges.push(arrayOfRucksucks[2][m]);
+                arrayOfRucksucks.splice(0, 3);
+                break;
+              }
+            }
+          }
+        }
+      }
     }
   }
 }
 
-console.log(sumatoria2);
-console.log(contador);
+findingbadge();
 
-console.log(12898 + 11767);
+// console.log(arrayOfRucksucks[0].length);
+console.log(arrayOfBadges);
