c9d89b092e6087d530b049c7e127fbc0a2b2d4b1
joaquin1990 <joaquingarese@gmail.com>
2022-12-27 11:47:20 -0300

    Day 5 completed

--- day5/src/firstPart.js
+++ day5/src/firstPart.js
@@ -1,16 +1,73 @@
 import fs from "fs";
+// move 6 from 4 to 3
 
-const one = ["J", "H", "G", "M", "Z", "N", "T", "F"];
-const two = ["V", "W", "J"];
-const three = ["G", "V", "L", "J", "B", "T", "H"];
-const four = ["B", "P", "J", "N", "C", "D", "V", "L"];
-const five = ["F", "W", "S", "M", "P", "R", "G"];
-const six = ["G", "H", "C", "F", "B", "N", "V", "M"];
-const seven = ["D", "H", "G", "M", "R"];
-const eight = ["H", "N", "M", "V", "Z", "D"];
-const nine = ["G", "N", "F", "H"];
+const stacks = {
+  0: ["Start!"],
+  1: ["J", "H", "G", "M", "Z", "N", "T", "F"],
+  2: ["V", "W", "J"],
+  3: ["G", "V", "L", "J", "B", "T", "H"],
+  4: ["B", "P", "J", "N", "C", "D", "V", "L"],
+  5: ["F", "W", "S", "M", "P", "R", "G"],
+  6: ["G", "H", "C", "F", "B", "N", "V", "M"],
+  7: ["D", "H", "G", "M", "R"],
+  8: ["H", "N", "M", "V", "Z", "D"],
+  9: ["G", "N", "F", "H"],
+};
 
 const data = fs.readFileSync("./info.txt", { encoding: "utf8", flag: "r" }).trim().split("\n");
-console.log(data);
 
-// Watch the example of day 4, try to use those functions (map, etc)
+// Clean information:
+// - Put inside an array each statement
+const cleanedArray = [];
+for (let x = 0; x < data.length; x++) {
+  let newArray = data[x].split(",");
+  let anotherArray = newArray[0].split(" ");
+  for (let i = 0; i < anotherArray.length; i++) {
+    if (anotherArray[i] === "move" || anotherArray[i] === "from" || anotherArray[i] === "to") {
+      anotherArray.splice(i, 1);
+    }
+  }
+  cleanedArray.push(anotherArray);
+}
+
+// Read instructions
+function readInstructions() {
+  let origin;
+  let destiny;
+  let quantity;
+  for (let x = 0; x < cleanedArray.length; x++) {
+    origin = Number(cleanedArray[x][1]);
+    destiny = Number(cleanedArray[x][2]);
+    quantity = Number(cleanedArray[x][0]);
+    // Movement:
+    for (let i = 1; i < quantity + 1; i++) {
+      let toMove = stacks[origin].pop();
+      stacks[destiny].push(toMove);
+    }
+  }
+}
+
+// readInstructions();
+
+function readInstructions2() {
+  let origin;
+  let destiny;
+  let quantity;
+  for (let x = 0; x < cleanedArray.length; x++) {
+    let actualStackLength = 0;
+    origin = Number(cleanedArray[x][1]);
+    destiny = Number(cleanedArray[x][2]);
+    quantity = Number(cleanedArray[x][0]);
+    actualStackLength = stacks[origin].length;
+    // Movement:
+    // Conditional to see if the large of the array we are going to cut is same or more bigger than the part of the array we want to pass to the other stack:
+
+    let toMove = stacks[origin].splice(actualStackLength - quantity, quantity);
+    for (let f = 0; f < quantity; f++) {
+      stacks[destiny].push(toMove[f]);
+    }
+  }
+}
+
+readInstructions2();
+console.log(stacks);
